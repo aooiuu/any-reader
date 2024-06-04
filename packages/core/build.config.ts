da76b4240c41b468c09b4f1b49ae1c57d7ff99b7
aooiuu <aooiu@qq.com>
2024-06-04 19:40:54 +0800

    chore: 修改依赖

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -4,18 +4,6 @@ export default defineBuildConfig({
   entries: [
     'src/index',
   ],
-  externals: [
-    'axios',
-    'base64-js',
-    'cheerio',
-    'compressing',
-    'jsonpath-plus',
-    'pako',
-    'urlencode',
-    'xmldom',
-    'xpath.js',
-    'puppeteer',
-  ],
   clean: true,
   declaration: true,
   rollup: {
