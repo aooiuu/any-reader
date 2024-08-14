c3e5fd2db44c7dc521d3373985ab1f5d3eac88c2
Horis <8674809+821938089@users.noreply.github.com>
2024-08-14 21:26:16 +0800

    fix: 更正代码 (#53)

--- packages/legado/src/analyzer/CombineEvaluator.ts
+++ packages/legado/src/analyzer/CombineEvaluator.ts
@@ -35,7 +35,7 @@ export class CombineEvaluator {
     }
 
     toString(): string {
-      return this.evals.map((_eval) => _eval.toString()).join(' && ');
+      return this.evals.map((_eval) => _eval.toString()).join('&&');
     }
   };
 
@@ -72,7 +72,7 @@ export class CombineEvaluator {
     }
 
     toString(): string {
-      return this.evals.map((_eval) => _eval.toString()).join(' || ');
+      return this.evals.map((_eval) => _eval.toString()).join('||');
     }
   };
 
@@ -124,7 +124,7 @@ export class CombineEvaluator {
     }
 
     toString(): string {
-      return this.evals.map((_eval) => _eval.toString()).join(' %% ');
+      return this.evals.map((_eval) => _eval.toString()).join('%%');
     }
   };
 }
