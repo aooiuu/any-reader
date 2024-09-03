b81667ed16954959db3fb419e3f808776c840d76
aooiuu <aooiu@qq.com>
2024-09-03 20:32:41 +0800

    feat(cli): 添加测试规则字符串命令

--- packages/cli/src/command/analyze.ts
+++ packages/cli/src/command/analyze.ts
@@ -4,8 +4,9 @@ import { createAnalyzerManager } from '@any-reader/core';
 export default (program: Command) => {
   program
     .command('analyze')
-    .argument('<rule>', '')
-    .argument('[result]', '', '')
+    .description('测试规则字符串')
+    .argument('<rule>', '规则字符串, 比如 `@js:1+1@js:result * result`')
+    .argument('[result]', 'result变量', '')
     .action(async (rule, result) => {
       const res = await createAnalyzerManager().getString(rule, result);
       console.log(res);
