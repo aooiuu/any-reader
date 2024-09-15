983c28e6dd91e9af4cf5f89af9cf06ee99be7c41
aooiuu <aooiu@qq.com>
2024-09-15 22:07:41 +0800

    docs: 规则测试状态保存

--- docs/.vitepress/components/RulePlay/index.vue
+++ docs/.vitepress/components/RulePlay/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div class="details custom-block !p-10px">
-      <input class="w-full" v-model="inputText" placeholder="这里输入原始文本, 比如网页源码" />
+      <textarea rows="5" class="w-full bg-[transparent]" v-model="inputText" placeholder="这里输入原始文本, 比如网页源码" />
     </div>
+
+    <div class="text-14 flex items-center">
+      <input type="checkbox" id="isList" name="isList" v-model="isList" />
+      <label for="isList">列表规则</label>
+    </div>
+
     <div class="details custom-block !p-10px">
       <input class="w-full" v-model="rule" placeholder="这里输入规则, 比如 xpath, jsonpath 等等组合" />
     </div>
@@ -16,7 +22,8 @@
 
 
 <script setup>
-import { ref, watch, nextTick } from 'vue'
+import { ref, watch, nextTick, onMounted } from 'vue'
+import { encode, decode } from 'js-base64';
 import { createAnalyzerManager } from '@any-reader/core/browser'
 
 const analyzerManager = createAnalyzerManager()
@@ -24,10 +31,44 @@ const analyzerManager = createAnalyzerManager()
 const inputText = ref('')
 const rule = ref('')
 const outputText = ref('')
+const isList = ref(false)
 
-watch([inputText, rule], v => {
+watch([inputText, rule, isList], v => {
+
+
   nextTick(async () => {
-    outputText.value = await analyzerManager.getString(rule.value, inputText.value)
+    outputText.value =
+      isList.value ?
+        JSON.stringify(await analyzerManager.getElements(rule.value, inputText.value))
+        : await analyzerManager.getString(rule.value, inputText.value)
   })
+
+  try {
+    const newHash = decodeURI(encode(JSON.stringify({
+      inputText: inputText.value,
+      rule: rule.value,
+      isList: isList.value,
+    })))
+    history.replaceState({}, '', '#' + newHash)
+
+  } catch (error) {
+    console.error(error);
+
+  }
+})
+
+onMounted(() => {
+  const hash = location.hash.slice(1)
+  if (hash) {
+    try {
+      const data = JSON.parse(decodeURI(decode(hash)))
+      inputText.value = data.inputText
+      rule.value = data.rule
+      isList.value = data.isList
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 })
 </script>
